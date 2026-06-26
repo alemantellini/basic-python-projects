0cabfbe41a66a429e2e200ccad62698f234fbbe9
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-06-26 19:42:01 +0000

    palabras

---
+++ el_ahorcado.py
@@ -0,0 +1,12 @@
+from palabras import palabras
+
+def obtener_palabra_válida(lista_palabras):
+    # Seleccionar una palabra al azar de la lista
+
+def ahorcado():
+    print("=" * 60)
+    print("¡Bienvenido(a) al juego del ahorcado!")
+    print("=" * 60)
+
+    palabra = obtener_palabra_válida(palabras)
+    
